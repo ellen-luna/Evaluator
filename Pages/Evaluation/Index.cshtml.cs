5edd0c2428b3e71d7dfc44fa40f6a05e80fb29c5
Alex <107702621+SlimeyMoose@users.noreply.github.com>
2023-08-16 11:06:18 +0300

    added to git ignore

--- Pages/Evaluation/Index.cshtml.cs
+++ Pages/Evaluation/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using Evaluator.Data;
+using Evaluator.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,26 @@ namespace Evaluator.Pages.Evaluation
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private readonly ApplicationDbContext _db;  //local instance of the database service
+
+		public List<EvaluationHead> objEvaluationHeadList;  //our UI front end will support looping through and displaying Categories retrieved from the database and stored in a List
+
+		public IndexModel(ApplicationDbContext db)  //dependency injection of the database service
+		{
+			_db = db;
+			objEvaluationHeadList = new List<EvaluationHead>();
+		}
+		public IActionResult OnGet()
+		{
+			//There are five major sets of IActionResult Types the can be returned
+			//1. Server Status Code Results
+			//2. Server Status Code and Object Results
+			//3. Redirect (to another webpage) Results
+			//4. File Results
+			//5. Content Results (like another Razor Web Page)
+			objEvaluationHeadList = _db.EvaluationHead.ToList();
+			return Page();    //render Page
+
+		}
+	}
 }
