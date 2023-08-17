9a092fc54ebaf2d76e0d7b97fc578a685345d3d7
ellen-luna <111672340+ellen-luna@users.noreply.github.com>
2023-08-17 10:46:41 +0300

    Update today17

--- Pages/Report/ReportSum.cshtml.cs
+++ Pages/Report/ReportSum.cshtml.cs
@@ -1,13 +1,26 @@
+using Evaluator.Data;
+using Evaluator.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Evaluator.Pages.Report
 {
-    public class ReportSumModel : PageModel
+	[BindProperties]
+	public class ReportSumModel : PageModel
     {
+		private readonly ApplicationDbContext _db;
+		public IEnumerable<Applicant> Applicant { get; set; }
+		public IEnumerable<ApplicantProf> ApplicantProfs {get; set;	}
 
-        public void OnGet()
+		public ReportSumModel(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public void OnGet()
         {
-        }
+			Applicant = _db.Applicant;
+			ApplicantProfs = _db.ApplicantProf;
+		}
     }
 }
